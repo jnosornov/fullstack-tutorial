e7730453523be798a6ac682635f4286ab2950657
Johnathan Osorno Valencia <jnosornov@gmail.com>
2021-02-21 12:29:15 -0500

    implement pagination

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -1,8 +1,27 @@
+const { paginateResults } = require('./utils')
+
 module.exports = {
   Query: {
-    launches: (_, __, ctx) => {
+    launches: async (_, { pageSize = 20, after }, ctx) => {
       const { dataSources } = ctx
-      return dataSources.launchAPI.getAllLaunches()
+      const allLaunches = await dataSources.launchAPI.getAllLaunches()
+
+      // we want these in reverse chronological order
+      allLaunches.reverse()
+      const launches = paginateResults({
+        after,
+        pageSize,
+        results: allLaunches,
+      })
+
+      return {
+        launches,
+        cursor: launches.length ? launches[launches.length - 1].cursor : null,
+        hasMore: launches.length
+          ? launches[launches.length - 1].cursor !==
+            allLaunches[allLaunches.length - 1].cursor
+          : false,
+      }
     },
     launch: (_, { id }, { dataSources }) =>
       dataSources.launchAPI.getLaunchById({ launchId: id }),
