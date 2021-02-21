c10a47a73a2a57dfaa5b6aae3b798d52d048eedf
Johnathan Osorno Valencia <jnosornov@gmail.com>
2021-02-20 22:29:39 -0500

    add non Query type resolvers

--- start/server/src/resolvers.js
+++ start/server/src/resolvers.js
@@ -8,4 +8,28 @@ module.exports = {
       dataSources.launchAPI.getLaunchById({ launchId: id }),
     me: (_, __, { dataSources }) => dataSources.userAPI.findOrCreateUser(),
   },
+  Mission: {
+    missionPatch: (mission, { size } = { size: 'LARGE' }) => {
+      return size === 'SMALL'
+        ? mission.missionPatchSmall
+        : mission.missionPatchLarge
+    },
+  },
+  Launch: {
+    isBooked: async (launch, _, ctx) => {
+      const { dataSources } = ctx
+      return dataSources.userAPI.isBookedOnLaunch({ launchId: launch.id })
+    },
+  },
+  User: {
+    trips: async (_, __, ctx) => {
+      // get ids of launches by user
+      const { dataSources } = ctx
+      const launchIds = await dataSources.userAPI.getLaunchIdsByUser()
+      if (!launchIds.length) return []
+
+      // look up lunches by ids
+      return dataSources.launchAPI.getLaunchesByIds({ launchIds }) || []
+    },
+  },
 }
